27398b6dbe71ba5af6852df59df59088159cf4f6
RainEggplant <i@raineggplant.com>
2020-10-17 20:10:13 +0800

    Change GLAD gl version to 4.3

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ GLFWwindow* initialize() {
   }
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
